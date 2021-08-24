51c6ed3ceab526734d7f4fcaaa3635a377092ff0
전상현 <sanghyun.jeon@seculetter.com>
2021-08-24 19:49:06 +0900

    윈도우 빌드스크립트에 기존 디렉토리 삭제 기능 추가

--- Build/BuildAll.bat
+++ Build/BuildAll.bat
@@ -3,8 +3,14 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
 SET SLNFILE=..\Src\_BuildAll.sln
 
+RD /S /Q .\Win32DebugMT
+RD /S /Q .\Win32ReleaseMT
+RD /S /Q .\x64DebugMT
+RD /S /Q .\x64ReleaseMT
+
 %MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=DebugMT;Platform=Win32
 %MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=ReleaseMT;Platform=Win32
 %MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=DebugMT;Platform=x64
 %MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=ReleaseMT;Platform=x64
+
 pause
