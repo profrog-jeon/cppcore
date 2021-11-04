afcde6077964bcaf74d8f0d09a3bf54c4ac2c77b
전상현 <sanghyun.jeon@architectkorea.com>
2021-11-04 16:09:34 +0900

    [BuildAll.bat] 8가지 빌드 타입을 모두 빌드하도록 수정

--- Build/BuildAll.bat
+++ Build/BuildAll.bat
@@ -3,10 +3,20 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
 SET SLNFILE=..\Src\_BuildAll.sln
 
+RD /S /Q .\Win32Debug
+RD /S /Q .\Win32Release
+RD /S /Q .\x64Debug
+RD /S /Q .\x64Release
+
 RD /S /Q .\Win32DebugMT
 RD /S /Q .\Win32ReleaseMT
 RD /S /Q .\x64DebugMT
 RD /S /Q .\x64ReleaseMT
+
+%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=Debug;Platform=Win32
+%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=Release;Platform=Win32
+%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=Debug;Platform=x64
+%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=Release;Platform=x64
 
 %MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=DebugMT;Platform=Win32
 %MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=ReleaseMT;Platform=Win32
