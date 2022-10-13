e4a19584b7bc358474c8ed64d0d761ca9f4a369f
전상현 <sanghyun.jeon@seculetter.com>
2022-10-13 23:01:54 +0900

    MODIFIED HashFunction Compile failure

--- Src/200_Common/HashFunction.h
+++ Src/200_Common/HashFunction.h
@@ -14,4 +14,15 @@ namespace core
 	std::string BuildFileHashA(E_HASH_TYPE nType, std::wstring strFilePath);
 	std::wstring BuildFileHashW(E_HASH_TYPE nType, std::string strFilePath);
 	std::wstring BuildFileHashW(E_HASH_TYPE nType, std::wstring strFilePath);
+	bool BuildFileHash(E_HASH_TYPE nType, std::string strFilePath, std::vector<BYTE>& vecHash);
+	bool BuildFileHash(E_HASH_TYPE nType, std::wstring strFilePath, std::vector<BYTE>& vecHash);
+
+	std::string BuildStringHash(E_HASH_TYPE nType, std::string strContext);
+	std::wstring BuildStringHash(E_HASH_TYPE nType, std::wstring strContext);
+	std::string BuildStringHashA(E_HASH_TYPE nType, std::string strContext);
+	std::string BuildStringHashA(E_HASH_TYPE nType, std::wstring strContext);
+	std::wstring BuildStringHashW(E_HASH_TYPE nType, std::string strContext);
+	std::wstring BuildStringHashW(E_HASH_TYPE nType, std::wstring strContext);
+	bool BuildStringHash(E_HASH_TYPE nType, std::string strContext, std::vector<BYTE>& vecHash);
+	bool BuildStringHash(E_HASH_TYPE nType, std::wstring strContext, std::vector<BYTE>& vecHash);
 }
