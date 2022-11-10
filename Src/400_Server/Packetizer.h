9a1300ced0c84ba8bd0e4647ecb097e22c6bf3c7
전상현 <profrog.jeon@gmail.com>
2022-11-10 15:10:09 +0900

    ADDED Overloded packetize function

--- Src/400_Server/Packetizer.h
+++ Src/400_Server/Packetizer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../300_Formatter/300_Formatter.h"
 #include "SyncTCPSocket.h"
 
 namespace core
@@ -36,58 +37,19 @@ namespace core
 	class CSocketForProtocol : public core::CSyncTCPSocket
 	{
 	public:
+		ECODE SendPacket(DWORD dwID, core::IFormatterObject* pPacket, DWORD dwTimeOut = 60000);
+		ECODE RecvPacket(DWORD dwID, core::IFormatterObject* pPacket, DWORD dwTimeOut = 60000);
+
 		template<typename T>
 		ECODE SendPacket(T* pPacket, DWORD dwTimeOut = 60000)
 		{
-			ECODE nRet = EC_SUCCESS;
-			try
-			{
-				std::vector<BYTE> vecPacket;
-				nRet = Packetize(T::ID, pPacket, vecPacket);
-				if (EC_SUCCESS != nRet)
-					throw exception_format(TEXT("Packetize failure, %d"), nRet);
-
-				nRet = Send(vecPacket.data(), vecPacket.size(), dwTimeOut);
-				if (EC_SUCCESS != nRet)
-					throw exception_format(TEXT("Packet sending failure, %d"), nRet);
-			}
-			catch (const std::exception& e)
-			{
-				core::Log_Error("%s", e.what());
-				return nRet;
-			}
-			
-			return EC_SUCCESS;
+			return SendPacket(T::ID, pPacket, dwTimeOut);
 		}
 
 		template<typename T>
 		ECODE RecvPacket(T* pPacket, DWORD dwTimeOut = 60000)
 		{
-			ECODE nRet = EC_SUCCESS;
-			try
-			{
-				ST_PACKET_HEADER header;
-				nRet = Peek(&header, sizeof(ST_PACKET_HEADER), dwTimeOut);
-				if (EC_SUCCESS != nRet)
-					throw core::exception_format(TEXT("Header Peeking faliure, %d"), nRet);
-
-				std::vector<BYTE> vecBody;
-				vecBody.resize(header.dwLen + sizeof(ST_PACKET_HEADER));
-				nRet = this->Recv(&vecBody[0], vecBody.size(), dwTimeOut);
-				if (EC_SUCCESS != nRet)
-					throw core::exception_format(TEXT("Body Recving failure, %d"), nRet);
-
-				nRet = Unpacketize(vecBody, T::ID, pPacket);
-				if (EC_SUCCESS != nRet)
-					throw core::exception_format(TEXT("Unpacketize failure, %d"), nRet);
-			}
-			catch (core::exception_format& e)
-			{
-				core::Log_Error("%s", e.what());
-				return nRet;
-			}
-
-			return EC_SUCCESS;
+			return RecvPacket(T::ID, pPacket, dwTimeOut);
 		}
 	};
 }
