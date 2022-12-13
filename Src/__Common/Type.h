fd17b9025ba25d3c80635796db048c06025a1744
전상현 <profrog.jeon@gmail.com>
2022-12-13 16:45:11 +0900

    ADDED tchar regex type.

--- Src/__Common/Type.h
+++ Src/__Common/Type.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <string>
+#include <regex>
 
 namespace std
 {
 #ifdef UNICODE
 	typedef std::wstring	tstring;
 	typedef std::string		ntstring;
+	typedef std::wregex		tregex;
+	typedef std::wsmatch	tsmatch;
 #else
 	typedef std::string		tstring;
 	typedef std::wstring	ntstring;
+	typedef std::regex		tregex;
+	typedef std::smatch		tsmatch;
 #endif
 }
 
