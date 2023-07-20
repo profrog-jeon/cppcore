600fa5a397ed1491b9c01094fa2c3a8f5816db43
전상현 <sanghyun.jeon@akdan.co.kr>
2023-07-20 17:13:28 +0900

    ADDED to ProtocolSuper SendBinary/RecvBinary.

--- Src/400_Server/ProtocolSuper.cpp
+++ Src/400_Server/ProtocolSuper.cpp
@@ -34,4 +34,60 @@ namespace core
 
 		return EC_SUCCESS;
 	}
+
+	inline static ECODE SendBinaryWorker(CSyncTCPSocket* pSocket, DWORD dwID, LPCVOID pData, size_t tDataSize, DWORD dwTimeOut)
+	{
+		std::vector<BYTE> vecPacket;
+
+		const size_t tHeaderSize = sizeof(ST_PACKET_HEADER);
+		vecPacket.resize(tHeaderSize + tDataSize);
+
+		ST_PACKET_HEADER* pHeader = (ST_PACKET_HEADER*)vecPacket.data();
+		pHeader->dwID = dwID;
+		pHeader->dwLen = (DWORD)tDataSize;
+		if (tDataSize)
+			memcpy(&vecPacket[tHeaderSize], pData, tDataSize);
+
+		return pSocket->Send(vecPacket.data(), vecPacket.size(), dwTimeOut);
+	}
+
+	ECODE CProtocolSuper::SendBinary(DWORD dwID, const std::vector<BYTE>& vecData, DWORD dwTimeOut)
+	{
+		return SendBinaryWorker(m_pSocket, dwID, vecData.data(), vecData.size(), dwTimeOut);
+	}
+
+	ECODE CProtocolSuper::SendBinary(DWORD dwID, LPCVOID pData, size_t tDataSize, DWORD dwTimeOut)
+	{
+		return SendBinaryWorker(m_pSocket, dwID, pData, tDataSize, dwTimeOut);
+	}
+
+	ECODE CProtocolSuper::RecvBinary(DWORD dwID, std::vector<BYTE>& vecData, DWORD dwTimeOut)
+	{
+		ECODE nRet = EC_SUCCESS;
+		try
+		{
+			ST_PACKET_HEADER header;
+			nRet = m_pSocket->Recv(&header, sizeof(header), dwTimeOut);
+			if (EC_SUCCESS != nRet)
+				throw exception_format(TEXT("header(ID:%u) recving failure, %d"), dwID, nRet);
+
+			nRet = EC_INVALID_DATA;
+			if (header.dwID != dwID)
+				throw exception_format(TEXT("header(ID:%u) not matched with:%u"), header.dwID, dwID);
+
+			if (header.dwLen)
+			{
+				vecData.resize(header.dwLen);
+				nRet = m_pSocket->Recv(&vecData[0], vecData.size(), dwTimeOut);
+				if (EC_SUCCESS != nRet)
+					throw exception_format(TEXT("body(ID:%u, Len:%u) recving failure, %d"), header.dwLen, nRet);
+			}
+		}
+		catch (const std::exception& e)
+		{
+			Log_Error("%s", e.what());
+			return nRet;
+		}
+		return EC_SUCCESS;
+	}
 }
