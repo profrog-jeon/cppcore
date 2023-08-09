82874f91186361a81327b195ecf6e7eea46ed39e
전상현 <sanghyun.jeon@akdan.co.kr>
2023-08-09 10:27:11 +0900

    [ServerTest] Bin 형식의 패키타이저 테스트

--- Test/ServerTest/ProtocolTest.cpp
+++ Test/ServerTest/ProtocolTest.cpp
@@ -36,28 +36,28 @@ TEST(ServerTest, BinProtocolTest)
 	EXPECT_EQ(original.strContext, restored.strContext);
 }
 
-class CTestConenction : public CSyncConnection
+class CSocketTestConenction : public CSyncConnection
 {
 	CSyncTCPSocket m_Socket;
 	CJsonProtocol m_Protocol;
 
 public:
-	CTestConenction(void)
+	CSocketTestConenction(void)
 	: CSyncConnection(&m_Socket)
 	, m_Socket()
 	, m_Protocol(&m_Socket)
 	{}
-	~CTestConenction(void)	{}
+	~CSocketTestConenction(void)	{}
 
-	void	OnConnect(void)
+	void OnConnect(void)
 	{
 	}
 
-	void	OnClose(void)
+	void OnClose(void)
 	{
 	}
 
-	void	OnRecv(void)
+	void OnRecv(void)
 	{
 		ST_TEST_PACKET packet;
 		EXPECT_EQ(EC_SUCCESS, m_Protocol.RecvPacket(&packet));
@@ -69,9 +69,9 @@ TEST(ServerTest, JsonProtocolSocketTest)
 {
 	ST_SYNCSERVER_INIT stInit;
 	stInit.wPort = 61503;
-	stInit.Connections.push_back(new CTestConenction());
-	stInit.Connections.push_back(new CTestConenction());
-	stInit.Connections.push_back(new CTestConenction());
+	stInit.Connections.push_back(new CSocketTestConenction());
+	stInit.Connections.push_back(new CSocketTestConenction());
+	stInit.Connections.push_back(new CSocketTestConenction());
 
 	CSyncServer server;
 	ASSERT_EQ(EC_SUCCESS, server.StartUp(stInit));
@@ -99,38 +99,66 @@ TEST(ServerTest, JsonProtocolSocketTest)
 	server.ShutDown();
 }
 
-TEST(ServerTest, JsonProtocolSocketPeekTest)
+
+class CBinTestConenction : public CSyncConnection
+{
+	CSyncTCPSocket m_Socket;
+	CJsonProtocol m_Protocol;
+
+public:
+	CBinTestConenction(void)
+		: CSyncConnection(&m_Socket)
+		, m_Socket()
+		, m_Protocol(&m_Socket)
+	{}
+	~CBinTestConenction(void) {}
+
+	void OnConnect(void)
+	{
+	}
+
+	void OnClose(void)
+	{
+	}
+
+	void OnRecv(void)
+	{
+		std::vector<BYTE> vecData;
+		EXPECT_EQ(EC_SUCCESS, m_Protocol.RecvBinary(2, vecData));
+		EXPECT_EQ(EC_SUCCESS, m_Protocol.SendBinary(2, vecData));
+	}
+};
+
+TEST(ServerTest, DISABLED_JsonProtocolSocketBinTest)
 {
 	ST_SYNCSERVER_INIT stInit;
 	stInit.wPort = 61503;
-	stInit.Connections.push_back(new CTestConenction());
-	stInit.Connections.push_back(new CTestConenction());
-	stInit.Connections.push_back(new CTestConenction());
+	stInit.Connections.push_back(new CBinTestConenction());
+	stInit.Connections.push_back(new CBinTestConenction());
+	stInit.Connections.push_back(new CBinTestConenction());
 
 	CSyncServer server;
 	ASSERT_EQ(EC_SUCCESS, server.StartUp(stInit));
 	Sleep(100);
 	{
-		ST_TEST_PACKET original;
-		original.a = 1;
-		original.b = 2;
-		original.strContext = TEXT("hello?");
-
 		CSyncTCPSocket client;
 		CJsonProtocol protocol(&client);
 		EXPECT_EQ(EC_SUCCESS, protocol.Connect("127.0.0.1", stInit.wPort, 1000));
-		EXPECT_EQ(EC_SUCCESS, protocol.SendPacket(&original));
 
-		ST_PACKET_HEADER header;
-		EXPECT_EQ(EC_SUCCESS, protocol.PeekHeader(header));
-		EXPECT_EQ(header.dwID, original.ID);
+		{
+			std::vector<BYTE> vecOriginal;
+			vecOriginal.resize(10);
+			memcpy(&vecOriginal[0], "ABCDEFGHIJ", 10);
+			EXPECT_EQ(EC_SUCCESS, protocol.SendBinary(2, vecOriginal));
 
-		ST_TEST_PACKET restored;
-		EXPECT_EQ(EC_SUCCESS, protocol.RecvPacket(&restored));
+			std::vector<BYTE> vecRestored;
+			EXPECT_EQ(EC_SUCCESS, protocol.RecvBinary(2, vecRestored));
 
-		EXPECT_EQ(original.a, restored.a);
-		EXPECT_EQ(original.b, restored.b);
-		EXPECT_EQ(original.strContext, restored.strContext);
+			EXPECT_EQ(vecOriginal.size(), vecRestored.size());
+
+			size_t tMinSize = std::min<size_t>(vecOriginal.size(), vecRestored.size());
+			EXPECT_EQ(0, memcmp(vecOriginal.data(), vecRestored.data(), tMinSize));
+		}
 
 		protocol.Close();
 	}
