6a4103e0cd3ed19643eecf6d09f95ec7e04d56ba
전상현 <sanghyun.jeon@akdan.co.kr>
2024-07-01 16:05:17 +0900

    MODIFIED NULL_DEREF

--- Src/200_Common/HashFunction.cpp
+++ Src/200_Common/HashFunction.cpp
@@ -7,6 +7,11 @@ namespace core
 	std::string BuildHash(E_HASH_TYPE nType, const std::vector<BYTE>& vecData)
 	{
 		HANDLE hHash = InitHash(nType);
+		if (NULL == hHash)
+		{
+			Log_Error(TEXT("InitHash(%s:%u) failure"), GetHashTypeString(nType), nType);
+			return "";
+		}
 		UpdateHash(hHash, vecData.data(), vecData.size());
 		return FinalHash(hHash);
 	}
